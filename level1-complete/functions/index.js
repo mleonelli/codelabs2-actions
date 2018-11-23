0f8c1248bd5cc572ef99d208286c851b5ce6870c
Mauro LEONELLI <mauro.leonelli@amadeus.com>
2018-11-23 09:47:11 +0100

    Added Suggestions to get the user name

--- level1-complete/functions/index.js
+++ level1-complete/functions/index.js
@@ -14,7 +14,7 @@
 'use strict';
 
 // Import the Dialogflow module from the Actions on Google client library.
-const {dialogflow} = require('actions-on-google');
+const {dialogflow, Permission, Suggestions} = require('actions-on-google');
 
 // Import the firebase-functions package for deployment.
 const functions = require('firebase-functions');
@@ -22,13 +22,37 @@ const functions = require('firebase-functions');
 // Instantiate the Dialogflow client.
 const app = dialogflow({debug: true});
 
+// Handle the Dialogflow intent named 'Default Welcome Intent'.
+app.intent('Default Welcome Intent', (conv) => {
+    conv.ask(new Permission({
+      context: 'Hi there, to get to know you better',
+      permissions: 'NAME'
+    }));
+  });
+
+// Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
+// agreed to PERMISSION prompt, then boolean value 'permissionGranted' is true.
+app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
+    if (!permissionGranted) {
+      conv.ask(`Ok, no worries. What's your favorite color?`);
+      conv.ask(new Suggestions('Blue', 'Red', 'Green'));
+    } else {
+      conv.data.userName = conv.user.name.display;
+      conv.ask(`Thanks, ${conv.data.userName}. What's your favorite color?`);
+      conv.ask(new Suggestions('Blue', 'Red', 'Green'));
+    }
+  });
+
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
 app.intent('favorite color', (conv, {color}) => {
     const luckyNumber = color.length;
-    // Respond with the user's lucky number and end the conversation.
-    conv.close('Your lucky number is ' + luckyNumber +'. Actually, the length of your favorite color...');
-});
+    if (conv.data.userName) {
+      conv.close(`${conv.data.userName}, your lucky number is ${luckyNumber}.`);
+    } else {
+      conv.close(`Your lucky number is ${luckyNumber}.`);
+    }
+  });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
