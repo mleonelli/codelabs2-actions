d0e239a055da770ec4d4afd6203426862f7061d5
Mauro LEONELLI <mleonelli@gmail.com>
2018-11-28 09:32:11 +0100

    Added support for storing permission

--- level1-complete/functions/index.js
+++ level1-complete/functions/index.js
@@ -24,10 +24,16 @@ const app = dialogflow({debug: true});
 
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
 app.intent('Default Welcome Intent', (conv) => {
+  const name = conv.user.storage.userName;
+  if (!name) {
+    // Asks the user's permission to know their name, for personalization.
     conv.ask(new Permission({
       context: 'Hi there, to get to know you better',
-      permissions: 'NAME'
+      permissions: 'NAME',
     }));
+  } else {
+    conv.ask(`Hi again, ${name}. What's your favorite color?`);
+  }
   });
 
 // Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
